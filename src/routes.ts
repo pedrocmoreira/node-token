4c2918113c34011090b464183c948ba070193549
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-12-12 14:30:49 -0300

    feat: adicionando rota de login para login

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { LoginController } from "./controllers/LoginController";
 import { UserController } from "./controllers/UserController";
 
 const routes = Router()
 
 routes.post('/user', new UserController().create);
+routes.post('/login', new LoginController().create);
 
 export default routes
